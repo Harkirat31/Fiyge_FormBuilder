51d0b2a2ce94d27128dfd6b080070efd1d9d4481
Harkirat Singh <harkiratsingh.tu@gmail.com>
2025-01-21 15:03:03 -0500

    Project initialised

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='panel'>
+      <div className='left-panel'></div>
+      <div className='main-panel'></div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/canvas-area/SidePanel.tsx
@@ -0,0 +1,8 @@
+const LeftPanel = ()=>{
+    return <>
+    <h1>Hello Left</h1>
+    </>
+}
+
+export default LeftPanel
+
